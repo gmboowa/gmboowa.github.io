7942eb2a07197952d77cd8b4e7eeccdcbdc8ba97
Gerald Mboowa <gmboowa@gmail.com>
2025-01-15 16:23:16 -0500

    Update scholar_source.rb

--- _bibliography/scholar_source.rb
+++ _bibliography/scholar_source.rb
@@ -2,13 +2,13 @@
 module Jekyll
   module Scholar
     module Utilities
-      # Define the scholar_source method only if it does not already exist
+      # Add the scholar_source method only if it does not already exist
       unless method_defined?(:scholar_source)
         def scholar_source(source)
-          # Check if the source is an absolute path and exists
+          # If source is an absolute path and exists, return it
           return source if source.start_with?('/') && File.exist?(source)
 
-          # Construct the path relative to the Jekyll source directory
+          # Otherwise, construct the path relative to the Jekyll source directory
           File.join(@config['source'], source)
         end
       end
