4468772f6012cc18501cb31b1507d63ca80b4835
Gerald Mboowa <gmboowa@gmail.com>
2025-01-15 17:55:46 -0500

    Update scholar_source.rb

--- _plugins/scholar_source.rb
+++ _plugins/scholar_source.rb
@@ -1,9 +1,16 @@
+# Extend Jekyll::Scholar::Utilities safely without redefining the Scholar module
 module Jekyll
   module Scholar
     module Utilities
-      def scholar_source(source)
-        return source if source.start_with?('/') && File.exist?(source)
-        File.join(@config['source'], source)
+      # Add scholar_source method only if it doesn't already exist
+      unless method_defined?(:scholar_source)
+        def scholar_source(source)
+          # Return source if it is an absolute path and exists
+          return source if source.start_with?('/') && File.exist?(source)
+
+          # Construct the path relative to the Jekyll source directory
+          File.join(@config['source'], source)
+        end
       end
     end
   end
