812a4b3faf1a7e06a6ca58d26450d2f11b161981
Gerald Mboowa <gmboowa@gmail.com>
2025-01-16 08:42:42 -0500

    Update scholar_patch.rb

--- _plugins/scholar_patch.rb
+++ _plugins/scholar_patch.rb
@@ -2,7 +2,12 @@ module Jekyll
   module Scholar
     module Utilities
       def scholar_source(source)
-        return source if source.start_with?('/') && File.exist?(source)
+        # Ensure source is an absolute path and exists
+        if source.start_with?('/') && File.exist?(source)
+          return source
+        end
+
+        # Otherwise, resolve relative to the configured source directory
         File.join(@config['source'], source)
       end
     end
