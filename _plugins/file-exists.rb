ab4ad7fd28ea35820f3c029290f93943748747ee
Gerald Mboowa <gmboowa@gmail.com>
2025-01-18 19:39:42 -0500

    Update file-exists.rb

--- _plugins/file-exists.rb
+++ _plugins/file-exists.rb
@@ -1,22 +1,23 @@
 module Jekyll
-  class FileExistsTag < Liquid::Tag
-    def initialize(tag_name, file_path, tokens)
-      super
-      @file_path = file_path.strip
-    end
+    class FileExistsTag < Liquid::Tag
 
-    def render(context)
-      # Resolve the full path of the file
-      site_source = context.registers[:site].config['source']
-      full_path = File.join(site_source, @file_path)
+        def initialize(tag_name, path, tokens)
+            super
+            @path = path
+        end
 
-      # Check if the file exists
-      File.exist?(full_path).to_s
-    rescue StandardError => e
-      Jekyll.logger.error "FileExistsTag Error:", e.message
-      "false"
+        def render(context)
+            # Pipe parameter through Liquid to make additional replacements possible
+            url = Liquid::Template.parse(@path).render context
+
+            # Adds the site source, so that it also works with a custom one
+            site_source = context.registers[:site].config['source']
+            file_path = site_source + '/' + url
+
+            # Check if file exists (returns true or false)
+            "#{File.exist?(file_path.strip!)}"
+        end
     end
-  end
 end
 
 Liquid::Template.register_tag('file_exists', Jekyll::FileExistsTag)
