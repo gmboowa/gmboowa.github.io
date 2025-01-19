46011a18a42756d0b723627e5c677c6b7b776d7e
Gerald Mboowa <gmboowa@gmail.com>
2025-01-19 11:23:27 -0500

    Describe the changes you made, e.g., Updated homepage layout

--- _plugins/details.rb
+++ _plugins/details.rb
@@ -1,34 +1,24 @@
-# Plugin to add support for {% details %} tags in Jekyll
-# This plugin enables collapsible <details> HTML elements in Jekyll using Liquid tags.
-# Inspired by: http://movb.de/jekyll-details-support.html
+# Code from http://movb.de/jekyll-details-support.html
 
 module Jekyll
-  class DetailsTag < Liquid::Block
-    def initialize(tag_name, markup, tokens)
-      super
-      @summary = markup.strip
-    end
-
-    def render(context)
-      # Get the site's Markdown converter
-      site = context.registers[:site]
-      converter = site.find_converter_instance(::Jekyll::Converters::Markdown)
-
-      # Convert the summary and block content to HTML
-      summary_html = @summary
-      content_html = converter.convert(super(context)).strip
-
-      # Return the rendered HTML for the <details> element
-      <<~HTML
-        <details>
-          <summary>#{summary_html}</summary>
-          #{content_html}
-        </details>
-      HTML
+    module Tags
+      class DetailsTag < Liquid::Block
+  
+        def initialize(tag_name, markup, tokens)
+          super
+          @caption = markup
+        end
+  
+        def render(context)
+          site = context.registers[:site]
+          converter = site.find_converter_instance(::Jekyll::Converters::Markdown)
+          caption = converter.convert(@caption).gsub(/<\/?p[^>]*>/, '').chomp
+          body = converter.convert(super(context))
+          "<details><summary>#{caption}</summary>#{body}</details>"
+        end
+  
+      end
     end
   end
-end
-
-# Register the custom Liquid tag
-Liquid::Template.register_tag('details', Jekyll::DetailsTag)
-
+  
+  Liquid::Template.register_tag('details', Jekyll::Tags::DetailsTag)
